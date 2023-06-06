272318069848f317d195cc5adac5ea187bbd4dc9
zoddicus <rharrison@google.com>
2023-06-06 14:48:24 +0000

    Deploying to gh-pages from @ zoddicus/cts@480edec387e8cd5bf5934680050c59a3f7a01438 🚀

---
+++ src/common/framework/test_config.ts
@@ -0,0 +1,26 @@
+export type TestConfig = {
+  maxSubcasesInFlight: number;
+  testHeartbeatCallback: () => void;
+  noRaceWithRejectOnTimeout: boolean;
+
+  /**
+   * Logger for debug messages from the test framework
+   * (that can't be captured in the logs of a test).
+   */
+  frameworkDebugLog?: (msg: string) => void;
+
+  /**
+   * Controls the emission of loops in constant-evaluation shaders under
+   * 'webgpu:shader,execution,expression,*'
+   * FXC is extremely slow to compile shaders with loops unrolled, where as the
+   * MSL compiler is extremely slow to compile with loops rolled.
+   */
+  unrollConstEvalLoops: boolean;
+};
+
+export const globalTestConfig: TestConfig = {
+  maxSubcasesInFlight: 500,
+  testHeartbeatCallback: () => {},
+  noRaceWithRejectOnTimeout: false,
+  unrollConstEvalLoops: false,
+};
