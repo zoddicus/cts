add602f11ff1484eb4aa7a09e261b1f8294a8830
zoddicus <rharrison@google.com>
2024-01-22 15:25:10 +0000

    Deploying to gh-pages from @ zoddicus/cts@ea9712a9bdc8ef20c452d64b08f2fd8791196f7d 🚀

--- out/webgpu/listing.js
+++ out/webgpu/listing.js
@@ -254,6 +254,15 @@ export const listing = [
       "operation",
       "memory_sync",
       "texture",
+      "readonly_depth_stencil"
+    ]
+  },
+  {
+    "file": [
+      "api",
+      "operation",
+      "memory_sync",
+      "texture",
       "same_subresource"
     ]
   },
@@ -2414,6 +2423,26 @@ export const listing = [
       "call",
       "builtin",
       "dot"
+    ]
+  },
+  {
+    "file": [
+      "shader",
+      "execution",
+      "expression",
+      "call",
+      "builtin",
+      "dot4I8Packed"
+    ]
+  },
+  {
+    "file": [
+      "shader",
+      "execution",
+      "expression",
+      "call",
+      "builtin",
+      "dot4U8Packed"
     ]
   },
   {
