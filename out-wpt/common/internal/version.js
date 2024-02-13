fc49488e33db513643a06d7651b7d3673f04cf1a
zoddicus <rharrison@google.com>
2024-02-13 18:11:58 +0000

    Deploying to gh-pages from @ zoddicus/cts@b87bbf634bc50a8a8ddf4509de91fbe5a6415182 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '2920b0fdf77675a0a4b746700730b55462b47486';
+export const version = 'b87bbf634bc50a8a8ddf4509de91fbe5a6415182';
