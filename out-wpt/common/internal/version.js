289b5308e730d21890760d7afd56d101707e9ef1
zoddicus <rharrison@google.com>
2024-10-22 16:11:56 +0000

    Deploying to gh-pages from @ zoddicus/cts@44754dbf52ca21ac5b46d593b955884b4ce0ec35 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '9387aa0b91cbf7cb985f3ddc9e7f24030cc879ae';
+export const version = '44754dbf52ca21ac5b46d593b955884b4ce0ec35';
--- out/webgpu/capability_info.js
+++ out/webgpu/capability_info.js
@@ -821,13 +821,16 @@ export const kFeatureNameInfo =
   'depth-clip-control': {},
   'depth32float-stencil8': {},
   'texture-compression-bc': {},
+  'texture-compression-bc-sliced-3d': {},
   'texture-compression-etc2': {},
   'texture-compression-astc': {},
+  'texture-compression-astc-sliced-3d': {},
   'timestamp-query': {},
   'indirect-first-instance': {},
   'shader-f16': {},
   'rg11b10ufloat-renderable': {},
   'float32-filterable': {},
+  'float32-blendable': {},
   'clip-distances': {},
   'dual-source-blending': {}
 };
